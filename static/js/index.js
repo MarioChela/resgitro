6ac8237c6958b006f4768ca9ae490b8eddca70c9
MarioChela <66433497+MarioChela@users.noreply.github.com>
2021-04-13 12:58:56 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -9,7 +9,21 @@
   
 
   client = new Paho.MQTT.Client("maqiatto.com", 8883, "web_" + parseInt(Math.random() * 100, 10));
-
+// called when a message arrives
+  function onMessageArrived(message) {
+    var le=document.getElementById("Sensor");
+    var l=document.getElementById("Sensor1");
+    console.log("onMessageArrived:"+message.payloadString);
+    variables=(message.payloadString).split(("-"));
+  }
+  function mostrard(){
+    var dat=document.getElementById("info");
+    dat.innerHTML=variables[0];
+  }
+  function mostrar2(){
+    var dat=document.getElementById("info");
+    dat.innerHTML=variables[1];
+  }
   // set callback handlers
   client.onConnectionLost = onConnectionLost;
   client.onMessageArrived = onMessageArrived;
